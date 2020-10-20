3f6fee3e079602ac88f06ce4627ef0d436c210f6
turowskaja@gmail.com <turowskaja@gmail.com>
2020-10-20 19:48:58 +0300

    init: start morse-decoder task

--- src/index.js
+++ src/index.js
@@ -35,10 +35,48 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    'space':  ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+
+    for (i=0; i<expr.length; i=i+10){
+     arr.push(expr.slice(i, i+10));
+    }
+
+    let arr2 = [];
+
+    for(i=0; i<arr.length; i++){
+
+        let str = '';
+
+        for(j=0; j<arr[i].length; j= j+2){
+
+            if(arr[i].includes('*')){
+            arr2.push('space');
+            break;
+            } else if(arr[i].slice(j, j+2) === '10'){
+            str = str + '.';
+            } else if (arr[i].slice(j, j+2) === '11'){
+            str = str + '-';
+            }
+        }
+        arr2.push(str);
+    }
+
+    let arr3 = [];
+
+    for(i=0; i<arr2.length; i++){
+        for (let key in MORSE_TABLE){
+            if(arr2[i] === key){
+                arr3.push(MORSE_TABLE[key]);
+            }
+        }
+    }
+
+    return arr3.join('');
+
 }
 
 module.exports = {
